1d0c7b3c3055c4ca14865e618a9bf11e230aacbc
Mounika Harini <146058701+Mounikaharini@users.noreply.github.com>
2026-08-30 17:42:50 +0530

    Create Day1_Intro.java

---
+++ Day1_Intro.java
@@ -0,0 +1,57 @@
+package TheoryClasses;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		System.out.println("hi");
+		
+		byte var1 = 127;
+		System.out.println(var1);
+		
+		short var2 = 3598;
+		System.out.println(var2);
+		
+		int var3 = 574985;
+		System.out.println(var3);
+		
+		long var4 = 789890563434L;
+		System.out.println(var4);
+		
+		float var5 = 8458.4875469F;
+		System.out.println(var5);
+		
+		double var6 = 678909.8766566567;
+		System.out.println(var6);
+		
+		char var7 = 'z';
+		System.out.println(var7);
+		
+		boolean var8 = true;
+		System.out.println(var8);
+		
+		String var9 = "java class";
+		System.out.println(var9);
+		
+		//implicit
+		byte a = 20;
+		int b = a;
+		
+		//explicit
+		int c = 96;
+		byte d = (byte)c;
+		*/
+		//operators
+//		1.Arithmetic (+ , -, * , /, %)
+//		2.Assignment (=, += , -=, *= , /=, %=)
+//		3.Comparision (== , != , < , > , <= , >=)
+//		4.Logical (&& , || , !)
+//		5.Unary operators (+ , - , !)
+//		6.Increment / Decrement (a++ , ++a , a-- , --a)
+//		7.Bitwise (& , | , ~ , ^ , << , >>)
+//		8.Ternary (cond ? exp1 : exp 2)
+	
+	}
+
+}
